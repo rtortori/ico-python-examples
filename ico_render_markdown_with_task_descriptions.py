08faddf71622095f20f7c2a983cce7011908b461
rtortori <rtortori@cisco.com>
2022-07-25 18:23:34 +0200

    added categories and targets

--- ico_render_markdown_with_task_descriptions.py
+++ ico_render_markdown_with_task_descriptions.py
@@ -1,15 +1,16 @@
 import credentials
 from intersight.api import workflow_api
 from intersight.rest import ApiException
+import re
 
-markdown_header = '''| Task Name | Description  |
-|---|---|'''
+markdown_header = '''| Task Name | Category | Description | Targets |
+|---|---|---|---|'''
 
 def get_tasks_definitions(api_client):
     """ Gets the list of tasks """
     api_instance = workflow_api.WorkflowApi(api_client)
     filter_query='(Properties.ExternalMeta eq true) and (DefaultVersion eq true) and (SharedScope eq shared)'
-    select_query = 'Label,Description'
+    select_query = 'Label,Description,Tags'
     orderby_query = 'Label'
     try:
         api_response = api_instance.get_workflow_task_definition_list(filter=filter_query, select=select_query, orderby=orderby_query, top=0,_check_return_type=False)
@@ -23,9 +24,19 @@ api_client = credentials.config_credentials()
 # Get the list of Tasks
 tasks = get_tasks_definitions(api_client)
 
+
 # Render Markdown
 print(markdown_header)
 for task in tasks:
+    target_list = []
     desc = task['Description']
     label = task['Label']
-    print('| {} | {} |'.format(label, " ".join(desc.splitlines())))
+    for tag in task['Tags']:
+        if re.search(r'platformType', tag['Key']):
+            target_list.append(tag['Key'].split('.')[1])
+        if tag['Key'] == 'category':
+            category = tag['Value'] 
+    target_list_str = ""
+    for i in target_list:
+        target_list_str += i+" "
+    print('| {} | {} | {} | {} |'.format(label, category, " ".join(desc.splitlines()), target_list_str))
